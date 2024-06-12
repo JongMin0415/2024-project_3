a1c8adcd548c6ce5e810bbb7a1da8a7766ff4491
kms040224 <kms0402246103@gmail.com>
2024-06-12 09:15:52 +0900

    feat : " 0605"

--- UnityProject_2024_3_AB-main/UnityProject_24_3_AB/Assets/Scripts/Character/CharacterFSM.cs
+++ UnityProject_2024_3_AB-main/UnityProject_24_3_AB/Assets/Scripts/Character/CharacterFSM.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-
-public enum CharacterState
-{
+public enum CharacterState          //캐릭터 상태
+{ 
     Idle,
     WalkingToShelf,
     PickingItem,
@@ -13,16 +12,16 @@ public enum CharacterState
     PlacingItem
 }
 
-public class Timer
+public class Timer                  //커스텀 타이머
 {
-    private float timeRemining;
+    private float timeRemining;     //타이머 Float
 
-    public void Set(float time)
+    public void Set(float time)     //시간 설정
     {
         timeRemining = time;
     }
 
-    public void Update(float deltaTime)
+    public void Update(float deltaTime)     //업데이트 동기화
     {
         if (timeRemining > 0)
         {
@@ -30,7 +29,7 @@ public class Timer
         }
     }
 
-    public bool IsFinished()
+    public bool IsFinished()                //종료 체크
     {
         return timeRemining <= 0;
     }
@@ -38,39 +37,29 @@ public class Timer
 
 public class CharacterFSM : MonoBehaviour
 {
-    public CharacterState currentState;
-    private Timer timer;
+    public CharacterState currentState;                     //현재 캐릭터 선언
+    private Timer timer;                                    //타이머 선언
 
-    public NavMeshAgent agent;
+    public NavMeshAgent agent;                              //에이전트 붙이기
     public Transform target;
 
     public Transform Counter;
     public List<GameObject> targetPos = new List<GameObject>();
 
-    private static int NextPriority = 0;
-    private static readonly object priorityLock = new object();
+    private static int NextPriority = 0;                    //다음 에이전트위 우선 순위
+    private static readonly object priorityLock = new object(); //우선 순위 할당을 위한 동기화 객체
 
-    public bool isMoveDone = false;
+    public bool isMoveDone = false;                         //도착 판별용 Bool
 
-    public List<GameObject> myBox = new List<GameObject>();
+    public List<GameObject> mybox = new List<GameObject>();
 
     public int boxesToPick = 5;
     private int boxesPicked = 0;
 
-    void AssignPriority()
-    {
-        lock(priorityLock)
-        {
-            agent.avoidancePriority = NextPriority;
-            NextPriority = (NextPriority + 1) % 100;
-        }
-    }
-
-    // Start is called before the first frame update
     void Start()
     {
-        timer = new Timer();
-        currentState = CharacterState.Idle;
+        timer = new Timer();                                //타이머 할당
+        currentState = CharacterState.Idle;                 //캐릭터 상태 설정
         agent = GetComponent<NavMeshAgent>();
         AssignPriority();
     }
@@ -78,11 +67,11 @@ public class CharacterFSM : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timer.Update(Time.deltaTime);
+        timer.Update(Time.deltaTime);                       //타이머 업데이트와 동기화
 
-        if(!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+        if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
         {
-            if(!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+            if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
             {
                 isMoveDone = true;
             }
@@ -107,89 +96,103 @@ public class CharacterFSM : MonoBehaviour
                 break;
         }
     }
-
-    void ChangeState(CharacterState nextState, float waitTime = 0.0f)
-     {
-         currentState = nextState;
-         timer.Set(waitTime);
-     }
-
-    void MoveToTarget()
+    void AssignPriority()
     {
-        isMoveDone = false;
-
-        if(target != null)
+        lock (priorityLock)         //동기화 블록을 사용하여 우선 순위를 할당
         {
-            agent.SetDestination(target.position);
+            agent.avoidancePriority = NextPriority;
+            NextPriority = (NextPriority + 1) % 100;        //NavMeshAgent 우선순위 범위는 0 ~ 99
         }
     }
 
-    void Idle()
-    {
-        if(timer.IsFinished())
-        {
-            target = targetPos[Random.Range(0, targetPos.Count)].transform;
-            MoveToTarget();
-            ChangeState(CharacterState.WalkingToShelf, 2.0f);
-        }
-    }
 
-    void WalkingToShelf()
-    {
-        if (timer.IsFinished() && isMoveDone)
-        {
-            ChangeState(CharacterState.PickingItem, 2.0f);
-            boxesPicked = 0;
-        }
-    }
 
-    void PickingItem()
-    {
-        if (timer.IsFinished())
+
+        // Start is called before the first frame update
+
+        void MoveToTarget()
         {
-            if(boxesPicked < boxesToPick)
+            isMoveDone = false;
+
+            if (target != null)
             {
-                GameObject box = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                myBox.Add(box);
-                box.transform.parent = gameObject.transform;
-                box.transform.localEulerAngles = Vector3.zero;
-                box.transform.localPosition = new Vector3(0, boxesPicked * 2f, 0);
-
-                boxesPicked++;
-                timer.Set(0.5f);
+                agent.SetDestination(target.position);
             }
-            else
+        }
+
+        void ChangeState(CharacterState nextstate, float waitTime = 0.0f)       //FSM Stat 전환시 다음 스테이트의 타이머 시간 설정
+        {
+            currentState = nextstate;
+            timer.Set(waitTime);
+        }
+
+        void Idle()
+        {
+            if (timer.IsFinished())
             {
-                target = Counter;
+                target = targetPos[Random.Range(0,targetPos.Count)].transform;
                 MoveToTarget();
-                ChangeState(CharacterState.WalkingToCounter, 2.0f);
+                ChangeState(CharacterState.WalkingToShelf, 2.0f);
             }
         }
-    }
 
-    void WalkingToCounter()
-    {
-        if (timer.IsFinished() && isMoveDone)
+        void WalkingToShelf()
         {
-            ChangeState(CharacterState.PlacingItem, 2.0f);
+            if (timer.IsFinished() && isMoveDone)
+            {
+                ChangeState(CharacterState.PickingItem, 2.0f);
+                boxesPicked = 0;
+            }
         }
-    }
 
-    void PlacingItem()
-    {
-        if (timer.IsFinished())
+        void PickingItem()
         {
-            if(myBox.Count != 0)
+            if (timer.IsFinished())
             {
-                myBox[0].transform.position = Counter.transform.position;
-                myBox[0].transform.parent = Counter.transform;
-                myBox.RemoveAt(0);
-                timer.Set(0.1f);
-            }
-            else
-            {
-                ChangeState(CharacterState.Idle, 2.0f);
+                if(boxesPicked < boxesToPick) 
+                { 
+                    GameObject box = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    mybox.Add(box);
+                    box.transform.parent = gameObject.transform;
+                    box.transform.localEulerAngles = Vector3.zero;
+                    box.transform.localPosition = new Vector3(0, boxesPicked * 2f, 0);
+
+                    boxesPicked++;
+                    timer.Set(0.5f);
+                }
+                else
+                {
+                    target = Counter;
+                    MoveToTarget();
+                    ChangeState(CharacterState.WalkingToCounter, 2.0f);
+                }              
             }
         }
-    }
+
+        void WalkingToCounter()
+        {
+            if (timer.IsFinished())
+            {
+                ChangeState(CharacterState.PlacingItem, 2.0f);
+            }
+        }
+
+        void PlacingItem()
+        {
+            if (timer.IsFinished())
+            {
+                if (mybox.Count != 0)
+                {
+                    mybox[0].transform.position = Counter.transform.position;
+                    mybox[0].transform.parent = Counter.transform;
+                    mybox.RemoveAt(0);
+                    timer.Set(0.1f);
+                }
+                else
+                {
+                    ChangeState(CharacterState.Idle, 2.0f);
+                }
+            }
+        }
+    
 }
